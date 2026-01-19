b352b6e5dafcfdb174830185c93dcacd3aefb224
kanishkagarud26 <kanishka.garud@mitaoe.ac.in>
2026-01-19 13:29:50 +0530

    new changes updated

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
+  await app.listen(port);
+  console.log(`Application running on port ${port}`);
 }
 bootstrap();
